aba58458bac2a2a317df90308e8f321bdbdb0605
mariefontanel <marie.fontanel.pro@gmail.com>
2018-04-04 17:31:47 +0200

    create migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,15 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('city');
+            $table->dateTime('birthdate');
+            $table->integer('phone_number');
+            $table->boolean('entreprise');
+            $table->integer('siret');
+            $table->string('description');
+            $table->string('pitch_video');
+            $table->string('cv');
+            $table->string('avatar');
             $table->rememberToken();
             $table->timestamps();
         });
