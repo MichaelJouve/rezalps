d80a305a3fb08e2e203b409f9b39ceb4077810fa
damien <dubois.damien508@gmail.com>
2018-04-04 13:39:20 +0200

    correction of web.php

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,11 @@ Route::get('welcome2', 'HomeController@index')->name('welcome');
 Route::get('flux', 'HomeController@index')->name('flux');
 Route::get('publications', 'HomeController@publications')->name('publications');
 Route::get('cv', 'HomeController@cv')->name('cv');
-Route::get('medias', 'HomeController@index')->name('medias');
-Route::get('reseau', 'HomeController@index')->name('reseau');
-Route::get('parametres', 'HomeController@index')->name('parametres');
-Route::get('messagerie', 'HomeController@index')->name('messagerie');
-Route::get('cgu', 'HomeController@index')->name('cgu');
-Route::get('about-us', 'HomeController@index')->name('about-us');
+Route::get('medias', 'HomeController@medias')->name('medias');
+Route::get('reseau', 'HomeController@reseau')->name('reseau');
+Route::get('parametres', 'HomeController@parametres')->name('parametres');
+Route::get('messagerie', 'HomeController@messagerie')->name('messagerie');
+Route::get('cgu', 'HomeController@cgu')->name('cgu');
+Route::get('about-us', 'HomeController@about-us')->name('about-us');
 
 Auth::routes();
