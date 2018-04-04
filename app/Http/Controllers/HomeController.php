dd536190ce1a1c82e8707814217cb02b0aa7c2a4
mariefontanel <marie.fontanel.pro@gmail.com>
2018-04-04 13:54:53 +0200

    update controller and routes for the nav links

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,25 +11,34 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function inscription()
     {
         return view('inscription');
     }
+
     public function cv()
     {
         return view('cv');
     }
 
-    public function inscription()
-    {
-        return view('inscription');
-    }
     public function publications()
     {
         return view('publications');
     }
+
     public function flux()
     {
         return view('flux');
     }
+
+    public function medias()
+    {
+        return view('medias');
+    }
+
+    public function reseau()
+    {
+        return view('reseau');
+    }
+
 }
