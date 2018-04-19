2789e6af85318b705de3d7d91590aa861438391e
damien <dubois.damien508@gmail.com>
2018-04-19 11:10:09 +0200

    add route::post, modify Homecontrollers and postcontrollers

--- routes/web.php
+++ routes/web.php
@@ -13,26 +13,34 @@
 
 Route::get('/', 'HomeController@registration')->name('registration');
 
-Route::get('accueil', 'PostController@accueil')->name('accueil')->middleware('auth');
+Route::get('flux', 'PostController@flux')->name('flux');
 
-Route::get('user/{id}/publications', 'UserController@publications')->name('publications')->middleware('auth');
-Route::get('user/{id}/cv', 'UserController@cv')->name('cv')->middleware('auth');
-Route::get('user/{id}/network', 'UserController@network')->name('network')->middleware('auth');
-Route::get('user/{id}/medias', 'UserController@medias')->name('medias')->middleware('auth');
+Route::get('user/{id}/publications', 'UserController@publications')->name('user.publications');
+Route::get('user/{id}/cv', 'UserController@cv')->name('user.cv');
+Route::get('user/{id}/network', 'UserController@network')->name('user.network');
+Route::get('user/{id}/medias', 'UserController@medias')->name('user.medias');
 
-Route::get('user/cv', 'UserController@cv')->name('cv')->middleware('auth');
-Route::get('user/network', 'UserController@network')->name('network')->middleware('auth');
-Route::get('user/settings', 'UserController@settings')->name('settings')->middleware('auth');
+Route::get('user/cv', 'UserController@cv')->name('user.cv');
+Route::post('add-cv', 'UserController@create')->name('add-cv');
+Route::post('add-pitch', 'UserController@create')->name('add-pitch');
+Route::post('update-apropos', 'UserController@create')->name('update-apropos');
 
-Route::get('messaging', 'MailController@mail')->name('messaging')->middleware('auth');
+Route::get('user/network', 'UserController@network')->name('network');
 
+Route::get('user/settings', 'UserController@settings')->name('settings');
+Route::post('update-setting', 'UserController@create')->name('update-setting');
 
-Route::get('user/publications', 'PostController@publications')->name('publications')->middleware('auth');
+Route::get('messaging', 'MailController@mail')->name('messaging');
+Route::post('send-message', 'MailController@create')->name('send-message');
 
-Route::get('user/medias', 'MediaController@medias')->name('medias')->middleware('auth');
+Route::get('user/publications', 'PostController@publications')->name('publications');
+Route::post('add-post', 'PostController@create')->name('add-post');
+Route::post('add-comment', 'PostController@create')->name('add-comment');
+
+Route::get('user/medias', 'MediaController@medias')->name('medias');
 
 Route::get('cgu', 'HomeController@cgu')->name('cgu');
-Route::get('about-us', 'HomeController@about-us')->name('about-us');
-Route::get('legal_notice', 'HomeController@legal_notice')->name('legal_notice');
+Route::get('about-us', 'HomeController@aboutUs')->name('about-us');
+Route::get('legal-notice', 'HomeController@legalNotice')->name('legal-notice');
 
 Auth::routes();
