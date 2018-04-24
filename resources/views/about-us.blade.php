52c63ad479e9db5accd7d1c3b20c128d362b5546
damien <dubois.damien508@gmail.com>
2018-04-24 16:35:57 +0200

    creation of about-us html starting

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -1,5 +1,37 @@
 @extends('layouts.static')
 
 @section('content')
+    <main>
+        <section>
+        <div class="row">
+            <div class="col-6 offset-3">
+                <div>
+                    <p>Epik SAS, créée en Janvier 2018 est une équipe du Campus Numérique In The Alps Valence</p>
+                    <p>Notre Projet "Rezalps" est un réseau social nous servant de fil rouge tout au long de notre formation de Technicien Analyste Développeur</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6">
+                <h3>Marie</h3>
+                <img src="">
+            </div>
+            <div class="col-6">
+                <h3>Nicolas</h3>
+                <img src="">
+            </div>
+        </div>
 
+            <div class="row">
+                <div class="col-6">
+                    <h3>Damien</h3>
+                    <img src="/img/damiend.png">
+                </div>
+                <div class="col-6">
+                    <h3>Michael</h3>
+                    <img src="">
+                </div>
+            </div>
+        </section>
+    </main>
 @endsection
