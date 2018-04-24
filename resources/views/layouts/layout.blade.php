6782cd12a424ea65594a509a4212879b5709d262
Votre Michael <jouvemichael@gmail.com>
2018-04-24 12:21:56 +0200

    add layout general

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Connexion REZALPS</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href={{URL::asset('main.css')}}>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="REZALPS reseau social dédié au metier de developpeur informatique">
-    <meta >
-    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
-</head>
+@extends('layouts.general')
 
 <body class="body">
 
