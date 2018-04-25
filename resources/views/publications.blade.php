c515b13d319a70161cd320ab27130562089af790
mariefontanel <marie.fontanel.pro@gmail.com>
2018-04-25 11:22:42 +0200

    post publication area added with shared include directory

--- resources/views/publications.blade.php
+++ resources/views/publications.blade.php
@@ -20,17 +20,7 @@
                 <div class="card-deck"> <!-- PUBLICATION 0 -->
                     <div class="card">
                         <div class="card-body">
-                            <form action="{{route('add-post')}}" method="POST">
-                                @csrf
-                                <textarea name="publication" cols="100" rows="3" placeholder="Publier ..."></textarea>
-                                <div class="upload-btn-wrapper">
-                                    <button class="btn">Pièce jointe</button>
-                                    <input type="file" name="myfile"/>
-                                </div>
-                                <div class="alignement_droite">
-                                    <input type="submit" class="btn" value="Envoyer"/>
-                                </div>
-                            </form>
+                        @include('shared.postinput')
                         </div>
                     </div>
                 </div>
