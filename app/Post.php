04dd5434fb22a288ed169b5cd48072e7d2d0c4f7
mariefontanel <marie.fontanel.pro@gmail.com>
2018-04-25 10:06:25 +0200

    post publication function added

--- app/Post.php
+++ app/Post.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     //only thoses items can be filled.
-    protected $fillable = array('publication', 'title');
+    protected $fillable = array('publication');
 }
