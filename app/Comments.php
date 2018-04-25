62ae79e908d1e32ae2efb28786b224e1d83dc813
mariefontanel <marie.fontanel.pro@gmail.com>
2018-04-25 12:21:40 +0200

    added relationship between post comment models

--- app/Comments.php
+++ app/Comments.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comments extends Model
 {
-    //
+    /**
+     * Link with model Comments
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function post()
+    {
+        return $this->belongsTo('App\Post');
+    }
 }
--- app/Post.php
+++ app/Post.php
@@ -18,5 +18,10 @@ class Post extends Model
         return $this->belongsTo('App\User');
     }
 
+    public function comments()
+    {
+        return $this->hasMany('App\comments');
+    }
+
 
 }
