5b6f4894b854e6f534875d5e5c8df8885eb2f245
mariefontanel <marie.fontanel.pro@gmail.com>
2018-04-26 15:15:29 +0200

    Model Post to record user_id

--- app/Post.php
+++ app/Post.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     //only thoses items can be filled.
-    protected $fillable = ['publication'];
+    protected $fillable = ['publication', 'user_id'];
 
     /**
      * Link with model user
