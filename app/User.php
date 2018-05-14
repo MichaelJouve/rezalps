2105b02ead2aa28e54e302a777c33ed9a7548aa2
Votre Michael <jouvemichael@gmail.com>
2018-05-14 14:39:35 +0200

    add author avatar and name on post and comment

--- app/User.php
+++ app/User.php
@@ -39,7 +39,7 @@ class User extends Authenticatable
      */
     public function posts()
     {
-        return $this->hasMany('App\Post');
+        return $this->hasMany('App\Post')->orderBy('created_at', 'desc');
     }
 
     /**
@@ -48,7 +48,7 @@ class User extends Authenticatable
      */
     public function comments()
     {
-        return $this->hasMany('App\Comment');
+        return $this->hasMany('App\Comment')->orderBy('created_at', 'desc');
     }
 
     /**
