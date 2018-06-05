16a818a1f035c8586b3e0c30ee7042516de40dd3
Votre Michael <jouvemichael@gmail.com>
2018-06-05 15:22:55 +0200

    update route index

--- routes/api.php
+++ routes/api.php
@@ -12,15 +12,16 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your Api!
 |
 */
+Route::get('posts', 'Api\PostController@index')->name('api-posts-index');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::middleware('auth:api')->group(function () {
-    Route::get('posts', 'Api\PostController@index')->name('api-posts-index');
     Route::get('post/{post}', 'Api\PostController@show')->name('api-post-show');
     Route::post('posts', 'Api\PostController@store')->name('api-post-store');
     Route::put('posts/{post}', 'Api\PostController@update')->name('api-post-update');
     Route::delete('posts/{post}', 'Api\PostController@delete')->name('api-post-delete');
 });
+
